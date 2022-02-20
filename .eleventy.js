d34f6fda376be71b8e9cfa99e905fff2b8b819d9
Anthony Dillon <me@anthonydillon.com>
2022-02-20 11:39:17 +0000

    Add robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/CNAME")
+  eleventyConfig.addPassthroughCopy("./src/robots.txt")
   return {
     dir: {
       input: "src",
