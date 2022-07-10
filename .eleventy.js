11ed4d6f887ea42e5881bc6feec5d22ac934fdf1
Anthony Dillon <me@anthonydillon.com>
2022-07-10 20:36:20 +0100

    Add rss (#1)
    * Add RSS to the site
    
    * Add RSS to the site

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateToRfc3339);
+  eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Anthony Dillon
-Website
+# Anthony Dillon website
+A portfolio and blog for a quiet developer called Anthony Dillon.
+
+## Usage
+1. Clone this repo
+2. Run `npm install`
+3. Start the site locally by running `npm run start`
+4. Visit http://localhost:8080/
